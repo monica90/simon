49361fb7ddde175f496bcf44842aa6c6d4b62ae9
monica90 <monicacentelles@gmail.com>
2016-01-26 20:39:16 +0100

    actu1

--- app/scripts/controllers/simon.js
+++ app/scripts/controllers/simon.js
@@ -8,25 +8,38 @@
  * Controller of the simonApp
  */
 angular.module('simonApp')
-  .controller('SimonCtrl', function () {
-    $(document).ready(function(){
-			$("#verde").click(function(){
-				$("#colorelegido").text("Verde");
-			})
-			$("#rojo").click(function(){
-				$("#colorelegido").text("Rojo");
-			})
-			$("#amarillo").click(function(){
-				$("#colorelegido").text("Amarillo");
-			})
-			$("#azul").click(function(){
-				$("#colorelegido").text("Azul");
-			})
+  .controller('SimonCtrl', function ($scope) {
 
-		})
+  	$scope.colores = [];
+  	$scope.titulo = 'Simón';
+
+
+  	$scope.play = function(color){
+  		$scope.colores.push(color);
+  	}
+
+  	$scope.reset = function(){
+  		$scope.colores = [];
+  	}
+
+  //   $(document).ready(function(){
+		// 	$("#verde").click(function(){
+		// 		$("#colorelegido").text("Verde");
+		// 	})
+		// 	$("#rojo").click(function(){
+		// 		$("#colorelegido").text("Rojo");
+		// 	})
+		// 	$("#amarillo").click(function(){
+		// 		$("#colorelegido").text("Amarillo");
+		// 	})
+		// 	$("#azul").click(function(){
+		// 		$("#colorelegido").text("Azul");
+		// 	})
+
+		// })
 		
-		function play(){
-			document.getElementById("audio");
-			audio.play();
-		}
+		// function play(){
+		// 	document.getElementById("audio");
+		// 	audio.play();
+		// }
   });
