cb18066b68b8109796c89720e1c139d28d5b07f6
monica90 <monicacentelles@gmail.com>
2016-01-28 19:23:31 +0100

    1

--- app/scripts/controllers/simon.js
+++ app/scripts/controllers/simon.js
@@ -12,14 +12,39 @@ angular.module('simonApp')
 
   	$scope.colores = [];
   	$scope.titulo = 'Simón';
+  	$scope.acumuladorverde = 0;
+  	$scope.acumuladorrojo = 0;
+  	$scope.acumuladorazul = 0;
+  	$scope.acumuladoramarillo = 0;
 
 
   	$scope.play = function(color){
   		$scope.colores.push(color);
+  		switch(color){
+  			case 'verde':
+	  			$scope.acumuladorverde = $scope.acumuladorverde +1; /*forma corta: $scope.acumulador++*/
+	  			break;
+	  		case 'rojo':
+	  			$scope.acumuladorrojo = $scope.acumuladorrojo +1;
+	  			break;
+	  		case 'azul':
+	  			$scope.acumuladorazul = $scope.acumuladorazul +1;
+	  			break;
+	  		case 'amarillo':
+	  			$scope.acumuladoramarillo = $scope.acumuladoramarillo +1;
+	  			break;
+  		}
+ 		/*otra manera: if(color=='verde'){$scope.acumuladorverde = $scope.acumuladorverde +1;}
+ 		Se usa swift en vez de poner muchas líneas iguales con if(color=='azul){...}, etc*/
+  		
   	}
 
   	$scope.reset = function(){
   		$scope.colores = [];
+  		$scope.acumuladorverde = 0;
+  		$scope.acumuladorrojo = 0;
+  		$scope.acumuladorazul = 0;
+  		$scope.acumuladoramarillo = 0;
   	}
 
   //   $(document).ready(function(){
